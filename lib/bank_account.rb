f974a90e99dd84a349be30904be3da22030b652f
Natasha Chernis <natashachernis@gmail.com>
2019-04-12 23:50:19 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,10 @@
 class BankAccount
 
-  attr_accessor :name, :balance
+  attr_accessor :name, :balance, :status
 
   def initialize(account_holder)
     @name = account_holder
     @balance = 1000
+    @status = "open"
   end
 end
