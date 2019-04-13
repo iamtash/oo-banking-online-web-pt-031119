ba997b44f164faa96237ab0da8a35b585e3632fd
Natasha Chernis <natashachernis@gmail.com>
2019-04-13 00:04:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,11 @@
 class Transfer
-  attr_accessor :status, :sender
+  attr_accessor :status, :sender, :receiver, :amount
 
   def initialize(sender, receiver, amount)
     @status = "pending"
     @sender = sender
+    @receiver = receiver
+    @amount = amount
   end
 
 end
