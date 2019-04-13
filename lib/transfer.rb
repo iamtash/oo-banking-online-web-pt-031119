4f385dbe48b9029f927ae7d9480dcd8d5f40ce9e
Natasha Chernis <natashachernis@gmail.com>
2019-04-13 00:12:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,11 +9,7 @@ class Transfer
   end
 
   def valid?
-    if self.sender.valid? == true && self.receiver.valid? == true
-      true
-    else
-      false
-    end
+    self.sender.valid? == true && self.receiver.valid? == true ? true : false
   end
 
 end
