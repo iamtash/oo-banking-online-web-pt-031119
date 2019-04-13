8ef1aa1df4aa25543f1c53239402b572901096f6
Natasha Chernis <natashachernis@gmail.com>
2019-04-13 00:35:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,7 @@ class Transfer
   def reverse_transfer
     if self.status == "complete"
       self.sender.balance += self.amount
-      self.receiver -= self.amount
+      self.receiver.balance -= self.amount
       self.status = "reversed"
     end
   end
